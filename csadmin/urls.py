dcd2e3b1895fbb94f1b60cc3b26b962924f49bd9
ingdemc <91351351+ingdemc@users.noreply.github.com>
2022-04-08 10:38:36 -0500

    heroku first commit

--- csadmin/urls.py
+++ csadmin/urls.py
@@ -18,7 +18,7 @@ from django.contrib import admin
 from django.urls import include,path
 from django.contrib.auth import views as auth_views
 
-from django.config import Settings
+from django.config import settings
 from django.config.urls.static import static
 
 urlpatterns = [
