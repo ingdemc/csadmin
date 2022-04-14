2c5f4f1d798306a0c8aa84f25730efbd3769cdc9
ingdemc <91351351+ingdemc@users.noreply.github.com>
2022-04-13 23:35:06 -0500

    requerimientos static

--- csadmin/settings/base.py
+++ csadmin/settings/base.py
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    
     'crispy_forms',
   ]
 
@@ -118,16 +119,16 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
-STATIC_URL = '../static/'
+STATIC_URL = 'static/'
+STATICFILES_DIRS = [
+    BASE_DIR / "static", 
+]
+
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 MEDIA_URL ='/media/'
 MEDIA_ROOT= os.path.join(BASE_DIR, 'static/media')
-
-os.makedirs(STATIC_ROOT, exist_ok=True)
-
-STATICFILES_DIR=(os.path.join(BASE_DIR, 'static'),)
 
 
 
